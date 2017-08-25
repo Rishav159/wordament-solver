04059b8e45effd76582e4708fc7bff24c8ea79d4
Rishav Agarwal <rishav.159@gmail.com>
2017-08-25 19:47:51 +0530

    Adds points

--- server.py
+++ server.py
@@ -162,8 +162,39 @@ def solution():
             positions.extend(x[1])
             visited[i][j] = False
     return words,positions
-
-
+points_array = {
+'A':2,
+'B':1,
+'C':3,
+'D':3,
+'E':1,
+'F':5,
+'G':4,
+'H':4,
+'I':2,
+'J':1,
+'K':6,
+'L':3,
+'M':4,
+'N':2,
+'O':2,
+'P':4,
+'Q':1,
+'R':2,
+'S':2,
+'T':2,
+'U':4,
+'V':6,
+'W':1,
+'X':1,
+'Y':5,
+'Z':8
+}
+def points(word):
+    score = 0
+    for letter in word:
+        score += points_array[letter]
+    return score
 @get('/')
 def index():
     return static_file('/index.html',root = './public')
@@ -177,6 +208,6 @@ def solve():
     final_words,final_positions = solution()
     print(len(final_words))
     print(len(final_positions))
-    final_words,final_positions = zip(*sorted(zip(final_words,final_positions), key = lambda x:len(x[0])))
+    final_words,final_positions = zip(*sorted(zip(final_words,final_positions), key = lambda x:points(x[0])))
     return json.dumps({'words': final_words, 'positions' : final_positions})
 run(host='localhost',port = 3000)
